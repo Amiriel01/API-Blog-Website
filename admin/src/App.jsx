cea830d5dc95a14ac64e768f8f490bec67ec4696
Sarah Quinney <quinney.sarah@gmail.com>
2023-10-09 14:08:11 -0500

    Homepage showing all articles' information correctly

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -6,41 +6,54 @@ import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
 import axios from "axios";
 import React from 'react';
 import Homepage from './Homepage';
+import Article from './Article';
 
 export default function App() {
-    //use states for methods (http verbs)
-    const [articles, setArticles] = useState([]);
+  //use states for methods (http verbs)
+  //get all articles and details
+  const [articles, setArticles] = useState([]);
 
-    async function getArticles() {
-      const response = await axios.get("http://localhost:3000/routers/articles").then((response) => {
-        setArticles(response.data);
-      });
-      
-    }
-
-    useEffect( () => {
-      const articles = getArticles()
-    }, []);
-
-    // async function postArticles() {
-    //   const baseURL = "http://localhost:3000/routers/articles";
-    //   const
-    // }
-
-
-    return (
-      <>
-        <div>
-        <Routes>
-        <Route path="*" element={<Homepage 
-          articles={articles}
-          setArticle={setArticles}
-        />} />
-        </Routes>
-        </div>
-
-      </>
-    )
+  async function getArticles() {
+    const response = await axios.get("http://localhost:3000/routers/articles").then((response) => {
+      setArticles(response.data);
+    });
   }
+
+  useEffect(() => {
+    const articles = getArticles()
+  }, []);
+
+  //get an article by id and its details
+  const [article, setArticle] = useState([]);
+
+  async function getArticle() {
+    const response = await axios.get("http://localhost:3000/routers/article/:id").then((response) => {
+      setArticle(response.data);
+    });
+  }
+
+  useEffect(() => {
+    const article = getArticle()
+  }, []);
+
+
+  return (
+    <>
+      <div>
+        <Routes>
+          <Route path="*" element={<Homepage
+            articles={articles}
+            setArticle={setArticles}
+          />} />
+          <Route path="article/:id" element={<Article
+            article={article}
+            setArticle={setArticle}
+          />} />
+        </Routes>
+      </div>
+
+    </>
+  )
+}
 
 
--- admin/src/Article.jsx
+++ admin/src/Article.jsx
@@ -4,23 +4,23 @@ import { useEffect } from "react";
 import App from "./App";
 import moment from "moment"
 
-export default function Homepage({ articles, setArticles }) {
-    console.log({ articles })
+export default function Article({ article, setArticle }) {
+    console.log({ article })
 
     return (
         <>
-            <h1 className="page-title">Admin Blog Page</h1>
+            <h1 className="page-title">Article Review Page</h1>
             <div className="article-container">
-                {articles.map((article) => {
+                {/* {article.map((article) => {
 
-                    return <div key={article._id}>
+                    return <div key={article._id}> */}
                         <p>{article.title}</p>
                         <p>{article.timestamp}</p>
                         <p>{article.article_text}</p>
                         <p>{article.comments}</p>
                         </div>
-                })}
-            </div>
+            {/* //     })}
+            // </div> */}
         </>
     )
 }
