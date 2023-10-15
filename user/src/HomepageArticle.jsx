cfcc7998c13a110ff1421e7794eb7ca09b0f79db
Sarah Quinney <quinney.sarah@gmail.com>
2023-10-15 11:44:55 -0500

    styled homepage and all articles, change colors later

---
+++ user/src/HomepageArticle.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import moment from "moment"
+import axios from 'axios'
+
+export default function Homepage() {
+
+    const [article, setArticle] = useState({});
+
+    async function getArticle() {
+        await axios.get(`http://localhost:3100/routers/article/652afa5821b153b3f4a772dc`).then((response) => {
+            // console.log(response)
+            setArticle(response.data);
+        });
+    }
+
+    useEffect(() => {
+        getArticle()
+    }, []);
+
+    return (
+        <>
+        
+        </>
+    )
+}
